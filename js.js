23273ed18b4cd6401499d0a9544cd36484bc0baa
Sebistorm <seba7843@stud.kea.dk>
2017-09-28 17:15:30 +0200

    effects on elements (scrollReveal)

--- js.js
+++ js.js
@@ -14,6 +14,10 @@
      });
  });
 
+
+ // anchor point
+
+
  // typing carousel
 
  var TxtRotate = function (el, toRotate, period) {
@@ -74,3 +78,60 @@
      css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #ffffff }";
      document.body.appendChild(css);
  };
+
+
+ // scrollReveal
+ window.sr = ScrollReveal();
+ sr.reveal('.navbar', {
+     duration: 2000,
+     origin: 'top'
+ });
+ sr.reveal('.mig', {
+     duration: 2000,
+     origin: 'bottom',
+     distance: '300px',
+     viewFactor: 0.30
+ });
+
+ sr.reveal('.hojre_om', {
+     duration: 2000,
+     origin: 'left',
+     distance: '300px',
+     viewFactor: 0.30
+ });
+
+ sr.reveal('.diagram_overblik', {
+     duration: 2000,
+     origin: 'bottom',
+     viewFactor: 0.5
+ });
+
+ sr.reveal('.raekke_1', {
+     duration: 2000,
+     origin: 'left',
+     distance: '300px',
+     viewFactor: 0.99
+ });
+
+ sr.reveal('.raekke_2', {
+     duration: 2000,
+     origin: 'left',
+     distance: '300px',
+     viewFactor: 0.99,
+     delay: 1500
+ });
+
+ sr.reveal('.raekke_3', {
+     duration: 2000,
+     origin: 'left',
+     distance: '300px',
+     viewFactor: 0.99,
+     delay: 3000
+ });
+
+ sr.reveal('#Kontakt div', {
+     duration: 2000,
+     origin: 'bottom',
+     distance: '300px',
+     viewFactor: 0.1,
+ });
